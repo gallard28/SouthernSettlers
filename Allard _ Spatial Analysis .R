148055752a23eabe1a41187c82db56a0b828c4fd
gallard28 <gallard@clemson.edu>
2018-11-06 08:48:48 -0500

    leaflet map done

--- Allard _ Spatial Analysis .R
+++ Allard _ Spatial Analysis .R
@@ -11,15 +11,34 @@ require(tidyr)
 require(ggplot2)
 library(leaflet)
 
+
 #Load Data
 load('inventor_df.RData')
 
 
 #subset data for NZ and UY
 inventor_UY_NZ_df<-inventor_df[inventor_df$country=="UY" | inventor_df$country=="NZ",]
+names(inventor_UY_NZ_df)
 
+head(inventor_df[inventor_df$country=="NZ",])
   
-#Visualize UY
-leaflet(inventor_UY_NZ_df) %>% 
-  addProviderTiles(providers$CartoDB.DarkMatterNoLabels) %>% 
-  addCircleMarkers(lng=inventor_df$longitude, lat=inventor_df$latitude, color="yellow")
+#Visualize data ###
+
+#Count of inventors per country 
+
+country_table<-inventor_UY_NZ_df %>% 
+  group_by(country) %>% 
+  count()
+  
+country_table
+
+#Set palette 
+pal<- colorFactor(c("white","blue"), domain= c("NZ", "UY"))
+  
+#Sample Map
+map<-leaflet(inventor_UY_NZ_df) %>% 
+  addProviderTiles(providers$CartoDB.DarkMatter) %>% 
+  addCircleMarkers(lng= ~longitude, lat= ~latitude, color= ~pal(country), stroke=FALSE, fillOpacity = 0.5)
+map
+
+
